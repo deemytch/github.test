10ec44629d219e303ebb21de20bdf3c76ccfee01
Димыч <aspamkiller@yandex.ru>
2013-12-26 17:22:06 +0200

    search by project with API is working

--- main.rb
+++ main.rb
@@ -3,6 +3,8 @@ require 'sass/plugin/rack'
 require 'sinatra/base'
 require 'json'
 require 'thin'
+require 'net/http'
+require 'uri'
 
 class Main < Sinatra::Base
 	enable :logging, :dump_errors, :sessions
@@ -10,11 +12,20 @@ class Main < Sinatra::Base
 	set :bind, 'localhost'
 
 	get '/' do
-		haml :index
+		haml :search
 	end
 
   get '/s' do
-    haml :search
+    p params
+    params[:p].gsub(/^https:\/\/github.com\/|^\//,'') =~ /^([^\/]+)\/([^\/]+)/
+    @user = $1
+    @proj = $2
+    uri = URI "https://api.github.com/repos/#{@user}/#{@proj}"
+    Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
+      @repo = JSON.parse( http.request(Net::HTTP::Get.new uri).body )
+    end
+    p @repo
+    haml :mined
   end
   
 	run! if app_file == $0
