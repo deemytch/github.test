4a154bce082fce1482e370fdc150b5234048163c
Димыч <aspamkiller@yandex.ru>
2013-12-27 14:12:56 +0200

    some error proof and list of avatars of the active users done

--- main.rb
+++ main.rb
@@ -39,43 +39,56 @@ class Main < Sinatra::Base
     
 #TODO: error handling needed
 #TODO: caching 202 -> 200 waiting also
-    stream do |out|
-      out << "<p id='streamed_tmp'>Checking activity "
-      Net::HTTP.start(uri[:orgs].host, uri[:orgs].port, :use_ssl => true) do |http|
-        # checking if that is an organization
-        resp = http.request(Net::HTTP::Get.new(uri[:orgs], hdr ))
-        p resp.code
-        if resp.code == '404'
-          @flash_msg = "'#{@org}' is not an organization"
-          redirect "/?message=#{@flash_msg}"
-        else
+    Net::HTTP.start(uri[:orgs].host, uri[:orgs].port, :use_ssl => true) do |http|
+      # checking if that is an organization
+      rq = Net::HTTP::Get.new(uri[:orgs], hdr )
+#          rq.basic_auth 'user', 'pass'
+      
+      resp = http.request(rq)
+      p resp.code
+      if resp.code == '404'
+        @flash_msg = "'#{@org}' is not an organization"
+        redirect "/?message=#{@flash_msg}"
+      elsif resp.code == '403' # rate limit exceeded
+        @flash_msg = "Rate limit exceeded. Try again in one hour."
+        redirect "/?message=#{@flash_msg}"
+      else
+        stream do |out|
+          out << "<p id='streamed_tmp'>Checking activity "
           @org_data = JSON.parse( resp.body )
           #get people
-          @people = JSON.parse( http.request(Net::HTTP::Get.new(uri[:people], hdr )).body )
+          rq = Net::HTTP::Get.new(uri[:people], hdr )
+#          rq.basic_auth 'user', 'pass'
+          
+          @people = JSON.parse( http.request(rq).body )
           out << "of the #{@people.size} members: "
-          puts "of the #{@people.size} members: "
+#          puts "of the #{@people.size} members: "
           #get people activity for today
           # and here`s a problem: github blocks more than 60 non-authorized requests per hour,
           # so I use If-Modified-Since and hoping to the best
           @active = []
           @people.each do |man|
             out << "#{man['login']} "
-            puts man['login']
-            req = http.request(Net::HTTP::Get.new("https://api.github.com/users/#{man['login']}/events", hdrtime))
+#            puts man['login']
+
+            rq = Net::HTTP::Get.new("https://api.github.com/users/#{man['login']}/events", hdrtime)
+#          rq.basic_auth 'user', 'pass'
+            
+            req = http.request(rq)
             if req.code == '200'
               @active << { :login => man['login'], :ava => man['avatar_url'], :url => man['html_url'], :events => JSON.parse( req.body ) }
               out << "* "
             end
           end
-          puts "Total #{@active.size} active members"
+#          puts "Total #{@active.size} active members"
           p @active
           #sort by that activity
           @active.sort!{|a,b| a[:events].size <=> b[:events].size }
           @active.reverse!
+          out << '</p>'
+          out << haml(:mined)
         end
       end
-      out << '</p>'
-      out << haml(:mined)
     end
   end
   
