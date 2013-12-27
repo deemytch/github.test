149adbf0932423567acdae4018388796a872380a
Димыч <aspamkiller@yandex.ru>
2013-12-27 13:50:21 +0200

    first up to 30 active users for today and mining progress

--- main.rb
+++ main.rb
@@ -26,54 +26,57 @@ class Main < Sinatra::Base
     @org = $1
     @proj = $2
     @base = 'https://api.github.com'
-    @today = Time.now
+    @today = Time.now.strftime("%a, %d %b %Y 00:00:00 GMT")
     uri = {}
     uri[:orgs] = URI "#{@base}/orgs/#{@org}"
     uri[:people] = URI "#{@base}/orgs/#{@org}/members"
     uri[:repo] = URI "#{@base}/repos/#{@org}/#{@proj}"
     uri[:code_frequency] = URI "#{@base}/repos/#{@org}/#{@proj}/stats/code_frequency"
     hdr = { 'Accept' => 'application/vnd.github.v3' }
+    hdrtime = { 'Accept' => 'application/vnd.github.v3', 'If-Modified-Since' => @today }
 #    @org = {}
 #    @people = []
     
 #TODO: error handling needed
 #TODO: caching 202 -> 200 waiting also
-    Net::HTTP.start(uri[:orgs].host, uri[:orgs].port, :use_ssl => true) do |http|
-      # checking if that is an organization
-      resp = http.request(Net::HTTP::Get.new(uri[:orgs], hdr ))
-      p resp.code
-      if resp.code == '404'
-        @flash_msg = "'#{@org}' is not an organization"
-        redirect "/?message=#{@flash_msg}"
-      else
-        @org_data = JSON.parse( resp.body )
-        #get people
-        @people = JSON.parse( http.request(Net::HTTP::Get.new(uri[:people], hdr )).body )
-        #get people activity for today
-        # and here`s a problem: github blocks too frequent non-authed requests
-        @people.each do |man|
-          man[:events] = JSON.parse( http.request(Net::HTTP::Get.new("https://api.github.com/users/#{man['name']}/events", hdr)).body ).select do |e|
-            t = Time.parse(e['created_at'])
-            t.year == @today.year && t.month == @today.month && t.day == @today.day
+    stream do |out|
+      out << "<p id='streamed_tmp'>Checking activity "
+      Net::HTTP.start(uri[:orgs].host, uri[:orgs].port, :use_ssl => true) do |http|
+        # checking if that is an organization
+        resp = http.request(Net::HTTP::Get.new(uri[:orgs], hdr ))
+        p resp.code
+        if resp.code == '404'
+          @flash_msg = "'#{@org}' is not an organization"
+          redirect "/?message=#{@flash_msg}"
+        else
+          @org_data = JSON.parse( resp.body )
+          #get people
+          @people = JSON.parse( http.request(Net::HTTP::Get.new(uri[:people], hdr )).body )
+          out << "of the #{@people.size} members: "
+          puts "of the #{@people.size} members: "
+          #get people activity for today
+          # and here`s a problem: github blocks more than 60 non-authorized requests per hour,
+          # so I use If-Modified-Since and hoping to the best
+          @active = []
+          @people.each do |man|
+            out << "#{man['login']} "
+            puts man['login']
+            req = http.request(Net::HTTP::Get.new("https://api.github.com/users/#{man['login']}/events", hdrtime))
+            if req.code == '200'
+              @active << { :login => man['login'], :ava => man['avatar_url'], :url => man['html_url'], :events => JSON.parse( req.body ) }
+              out << "* "
+            end
           end
+          puts "Total #{@active.size} active members"
+          p @active
+          #sort by that activity
+          @active.sort!{|a,b| a[:events].size <=> b[:events].size }
+          @active.reverse!
         end
-        #sort by that activity
-        @people.sort!{|a,b| a[:events].size <=> b[:events].size }
-        @active = []
-        count = 0 # showin only the first 10 brave programmers
-        @people.each do |man|
-          break if(man[:events].size == 0)||(count > 9)
-          @active << man
-          count+=1
-        end
-        # repo common data
-        # @repo = JSON.parse( http.request(Net::HTTP::Get.new(uri[:repo], hdr )).body )
-        # code frequency - additions and deletions per week
-        # @code_frik = JSON.parse( http.request(Net::HTTP::Get.new(uri[:code_frequency], hdr )).body )
       end
+      out << '</p>'
+      out << haml(:mined)
     end
-    # @people = { :login, :avatar_url, :events => { event } }
-    haml :mined
   end
   
 	run! if app_file == $0
