311a5c074abd8f3b267be83b58d0cf5dcc949c46
Димыч <aspamkiller@yandex.ru>
2013-12-27 08:10:35 +0200

    get people activity

--- main.rb
+++ main.rb
@@ -4,6 +4,7 @@ require 'json'
 require 'thin'
 require 'net/http'
 require 'uri'
+require 'time'
 
 class Main < Sinatra::Base
 	enable :logging, :dump_errors, :sessions
@@ -25,16 +26,15 @@ class Main < Sinatra::Base
     @org = $1
     @proj = $2
     @base = 'https://api.github.com'
+    @today = Time.now
     uri = {}
     uri[:orgs] = URI "#{@base}/orgs/#{@org}"
+    uri[:people] = URI "#{@base}/orgs/#{@org}/members"
     uri[:repo] = URI "#{@base}/repos/#{@org}/#{@proj}"
-    uri[:people] = URI "#{@base}/repos/#{@org}/#{@proj}/stats/contributors"
     uri[:code_frequency] = URI "#{@base}/repos/#{@org}/#{@proj}/stats/code_frequency"
     hdr = { 'Accept' => 'application/vnd.github.v3' }
-#    @repo = {}
-#    @repo['description'] = 'test descr'
+#    @org = {}
 #    @people = []
-#    @code_frik = []
     
 #TODO: error handling needed
 #TODO: caching 202 -> 200 waiting also
@@ -47,15 +47,31 @@ class Main < Sinatra::Base
         redirect "/?message=#{@flash_msg}"
       else
         @org_data = JSON.parse( resp.body )
+        #get people
+        @people = JSON.parse( http.request(Net::HTTP::Get.new(uri[:people], hdr )).body )
+        #get people activity for today
+        # and here`s a problem: github blocks too frequent non-authed requests
+        @people.each do |man|
+          man[:events] = JSON.parse( http.request(Net::HTTP::Get.new("https://api.github.com/users/#{man['name']}/events", hdr)).body ).select do |e|
+            t = Time.parse(e['created_at'])
+            t.year == @today.year && t.month == @today.month && t.day == @today.day
+          end
+        end
+        #sort by that activity
+        @people.sort!{|a,b| a[:events].size <=> b[:events].size }
+        @active = []
+        count = 0 # showin only the first 10 brave programmers
+        @people.each do |man|
+          break if(man[:events].size == 0)||(count >9)
+          @active << man
+        end
         # repo common data
         # @repo = JSON.parse( http.request(Net::HTTP::Get.new(uri[:repo], hdr )).body )
-        # contributors
-        # @people = JSON.parse( http.request(Net::HTTP::Get.new(uri[:people], hdr )).body )
         # code frequency - additions and deletions per week
         # @code_frik = JSON.parse( http.request(Net::HTTP::Get.new(uri[:code_frequency], hdr )).body )
       end
     end
-
+    # @people = { :login, :avatar_url, :events => { event } }
     haml :mined
   end
   
